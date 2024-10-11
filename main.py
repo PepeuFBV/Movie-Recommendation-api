d53eed3ef244b990e91acd468ad8b2c62a0de600
Pedro <pedrofigueiraboaviagem@gmail.com>
2024-10-11 11:05:01 -0300

    Changed model saving and reading

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import os
 from fastapi import FastAPI
 from surprise import SlopeOne, Reader, Dataset
 import pandas as pd
-import pickle
+import joblib
 
 app = FastAPI()
 
@@ -10,6 +11,10 @@ model_path = 'model/slope_one_model.pkl'
 # initialize the model and save it to a file
 @app.get("/init")
 async def init():
+
+    # ensure model directory exists
+    os.makedirs(os.path.dirname(model_path), exist_ok=True)
+
     # load the ratings data
     ratings = pd.read_csv('ratings_cleaned.csv')
 
@@ -21,8 +26,7 @@ async def init():
     model.fit(data.build_full_trainset())
 
     # save the model to a file
-    with open(model_path, 'wb') as f:
-        pickle.dump(model, f)
+    joblib.dump(model, model_path)
 
     return {"message": "Model initialized"}
 
@@ -31,7 +35,7 @@ async def init():
 async def predict(user_id: int, movie_id: int):
     # load the model from the file
     with open(model_path, 'rb') as f:
-        model = pickle.load(f)
+        model = joblib.load(f)
 
     # make a prediction
     prediction = model.predict(user_id, movie_id)
