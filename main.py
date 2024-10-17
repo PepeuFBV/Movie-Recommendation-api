f13c0bf3e1f3bbbf84d5dbae1427e7cd2a9fa7ad
Pedro <pedrofigueiraboaviagem@gmail.com>
2024-10-17 05:33:42 -0300

    Api fixes

--- main.py
+++ main.py
@@ -4,105 +4,112 @@ from fastapi import FastAPI
 from surprise import SlopeOne, Reader, Dataset
 import pandas as pd
 import joblib
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*", "http://localhost:3000"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
 model_path = 'model/slope_one_model.pkl'
-# initialize the model and save it to a file
+
 @app.get("/init")
 async def init():
-
-    # save the model to a file
+    print("Initializing model")
     joblib.dump(train_model(), model_path)
-
     return {"message": "Model initialized and trained"}
 
 
-# make a prediction using the model
+
 @app.get("/predict")
 async def predict(user_id: int, movie_id: int):
-    # load the model from the file
-    with open(model_path, 'rb') as f:
-        model = joblib.load(f)
-
-    # make a prediction
+    model = joblib.load(model_path)
     prediction = model.predict(user_id, movie_id)
-
     return {"prediction": prediction.est}
 
 
-# gets user id and movie id and rating and updates the model
+
+class RateMovieRequest(BaseModel):
+    user_id: str
+    movie_id: int
+    rating: float
+
 @app.post("/rate_movie")
-async def rate_movie(user_email: string, movie_id: int, rating: float):
-    user_id = get_user_id(user_email) # get the user id
-
-    # load the ratings data
+async def rate_movie(request: RateMovieRequest):
+    print("Received rating request: ", request)
+    user_id = get_user_id(request.user_id)
     ratings = pd.read_csv('ratings_10.csv')
-
-    # add the new rating to the ratings data
-    new_rating = pd.DataFrame({'userId': [user_id], 'movieId': [movie_id], 'rating': [rating]})
-    ratings = ratings.append(new_rating)
-
-    # save the new ratings data
+    new_rating = pd.DataFrame({'userId': [user_id], 'movieId': [request.movie_id], 'rating': [
+        request.rating]})
+    ratings = pd.concat([ratings, new_rating], ignore_index=True)
     ratings.to_csv('ratings_10.csv', index=False)
+    joblib.dump(train_model(), model_path)
+    print("Rating added and model retrained")
+    return {"message": "Rating added and model retrained"}
+
+class PredictMoviesRequest(BaseModel):
+    user_id: str
+    number_of_movies: int
+
+@app.post("/predict_movies")
+async def predict_movies(request: PredictMoviesRequest):
+    print("Predicting movies for user: ", request.user_id, " number of movies: ", request.number_of_movies)
+    user_id = get_user_id(request.user_id)
+    model = joblib.load(model_path)
+    # get all unique movie ids from the ratings file
+    movies_ids = pd.read_csv('ratings_10.csv')['movieId'].unique()
+    predictions = [model.predict(user_id, movie_id) for movie_id in movies_ids]
+    predictions.sort(key=lambda x: x.est, reverse=True)
+    top_movies = predictions[:request.number_of_movies]
+    top_movie_ids = [int(prediction.iid) for prediction in top_movies]  # Convert numpy.int64 to int
+    return top_movie_ids
+
 
 
 @app.get("/retrain")
 async def retrain():
-
-    # save the model to a file
     joblib.dump(train_model(), model_path)
-
     return {"message": "Model retrained"}
 
 
 
-
 def train_model():
-
-    # ensure model directory exists
-    os.makedirs(os.path.dirname(model_path), exist_ok=True) # will create the directory if it does not exist
-
-    # load the ratings data
+    os.makedirs(os.path.dirname(model_path), exist_ok=True)
     ratings = pd.read_csv('ratings_10.csv')
-
-    # define a Reader and load the data into a Dataset
     reader = Reader()
     data = Dataset.load_from_df(ratings[['userId', 'movieId', 'rating']], reader)
-
     model = SlopeOne()
     model.fit(data.build_full_trainset())
-
-    # save the model to a file
     joblib.dump(model, model_path)
-
     return model
 
 
+
 user_path = "users.csv"
-def get_user_id(user_email):
 
-    # ensure model directory exists
-    os.makedirs(os.path.dirname(model_path), exist_ok=True) # will create the directory if it does not exist
+def get_user_id(user_id):
+    # check if the file exists, if not, create it with the necessary columns
+    if not os.path.exists(user_path):
+        users_df = pd.DataFrame(columns=['user_id', 'model_id'])
+        users_df.to_csv(user_path, index=False)
 
-    # compare email to the emails in the users.csv file
+    os.makedirs(os.path.dirname(model_path), exist_ok=True)
     users = pd.read_csv(user_path)
-    user = users[users['email'] == user_email]
-    if len(user) == 0: # create a new user
-
-        # find the biggest userID value in ratings file
+    user = users[users['user_id'] == user_id]
+    if len(user) == 0:
         ratings = pd.read_csv('ratings_cleaned.csv')
         max_user_id = ratings['userId'].max()
-        new_user_id = max_user_id + 1
-
-        # save the new user to the users.csv file (email + new_user_id)
-        new_user = pd.DataFrame({'email': [user_email], 'userId': [new_user_id]})
-        users = users.append(new_user)
+        new_model_id = max_user_id + 1
+        new_user = pd.DataFrame({'user_id': [user_id], 'model_id': [new_model_id]})
+        users = pd.concat([users, new_user], ignore_index=True)
         users.to_csv(user_path, index=False)
-
-        user_id = new_user_id
-
+        model_id = new_model_id
     else:
-        user_id = user['userId'].values[0]
-
-    return user_id
+        model_id = user['model_id'].values[0]
+    return model_id
